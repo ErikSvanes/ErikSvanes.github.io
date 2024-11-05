840571da88d6d438cfcec680640af8456a1c231c
Erik Svanes <svanes.erik@gmail.com>
2024-11-05 13:59:32 -0500

    switching to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 
-import {
-    Box,
-    Text,
-    Flex,
-} from '@chakra-ui/react'
-
 function App() {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
 
     return (
-        <Box position='fixed' width='100%' height='100%' top='0' left='0' borderColor={"#ff0000"} borderWidth={1}>
-            <Flex>
-                <Text>
-                    Hello World
-                </Text>
-            </Flex>
-        </Box>
+        <div className='app flex items-center justify-center bg-gray-800'>
+            <h1 className='text-white text-xl'>
+                Now Using Tailwind CSS!
+            </h1>
+        </div>
     )
 }
 
